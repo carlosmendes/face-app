17e135a76d3206e6674c72e4042621882640583a
Carlos Mendes <carls.mendes@gmail.com>
2016-08-09 18:02:39 +0100

    improve facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,15 +27,31 @@ class User < ActiveRecord::Base
     # return user
 
     # Wiki Version
-	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-	    user.email = auth.info.email
-	    user.password = Devise.friendly_token[0,20]
-	    user.first_name = auth.info.first_name   # assuming the user model has a name
-	    user.last_name = auth.info.last_name   # assuming the user model has a name
-	    user.facebook_picture_url = auth.info.image # assuming the user model has an image
-	    user.token = auth.credentials.token
-	    user.token_expiry = Time.at(auth.credentials.expires_at)
-    end
+	  # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	  #   user.email = auth.info.email
+	  #   user.password = Devise.friendly_token[0,20]
+	  #   user.first_name = auth.info.first_name   # assuming the user model has a name
+	  #   user.last_name = auth.info.last_name   # assuming the user model has a name
+	  #   user.facebook_picture_url = auth.info.image # assuming the user model has an image
+	  #   user.token = auth.credentials.token
+	  #   user.token_expiry = Time.at(auth.credentials.expires_at)
+   #  end
+
+   user = User.find_by_email(auth.info.email)
+   user = User.new if user.nil?
+   user.email = auth.info.email
+   user.password = Devise.friendly_token[0,20] if user.encrypted_password.empty?
+   user.provider = auth.provider
+   user.uid = auth.uid
+   user.first_name = auth.info.first_name
+   user.last_name = auth.info.last_name
+   user.facebook_picture_url = auth.info.image
+   user.token = auth.credentials.token
+   user.token_expiry = Time.at(auth.credentials.expires_at)
+
+   user.save
+
+   return user
   end
 
 end
